2c28e486958b27595b1c03e97b6098012f9dec52
David <ds0130@uah.edu>
2026-07-15 20:12:30 -0500

    sync npm workspace

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -7,36 +7,42 @@ module.exports = {
     scheme: "yakquest",
     version: "1.0.0",
     orientation: "portrait",
+
     icon: "./images/icon.png",
+
     splash: {
       image: "./images/icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
+
     plugins: [
       [
         "expo-location",
         {
           locationAlwaysAndWhenInUsePermission:
             "Allow YakQuest to access your location while navigating rivers.",
+
           locationAlwaysPermission:
             "Allow YakQuest to access your location in the background while a trip is active.",
+
           locationWhenInUsePermission:
             "Allow YakQuest to access your location while using the app.",
+
           isAndroidBackgroundLocationEnabled: true,
         },
       ],
     ],
+
     android: {
       package: "com.yakquest.mobile",
+      versionCode: 1,
+
       adaptiveIcon: {
         foregroundImage: "./images/icon.png",
         backgroundColor: "#EAF7F7",
       },
-      "navigationBar": {
-        "backgroundColor": "#00000000",
-        "barStyle": "light-content"
-      },
+
       permissions: [
         "android.permission.ACCESS_COARSE_LOCATION",
         "android.permission.ACCESS_FINE_LOCATION",
@@ -44,14 +50,27 @@ module.exports = {
         "android.permission.FOREGROUND_SERVICE",
         "android.permission.FOREGROUND_SERVICE_LOCATION",
       ],
+
       config: {
         googleMaps: {
-          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+          apiKey:
+            process.env.GOOGLE_MAPS_API_KEY,
         },
       },
     },
+
+    androidNavigationBar: {
+      backgroundColor: "#00000000",
+      barStyle: "light-content",
+    },
+
     extra: {
-      apiUrl: process.env.EXPO_PUBLIC_API_URL,
+      apiUrl:
+        "https://api.yakquest.com",
+      eas: {
+        projectId:
+          "0ca4db72-c8e9-4b89-8b24-163903fc7e9d",
+      },
     },
   },
 };
